e2ff17a76f89d5dc23df6e4cd7a56261bea94fa3
kpatenio <katherinepatenio@gmail.com>
2018-02-05 14:52:48 -0700

    Added copyright

--- app/src/main/java/ca/ualberta/cs/patenio_subbook/SubAddMenuActivity.java
+++ app/src/main/java/ca/ualberta/cs/patenio_subbook/SubAddMenuActivity.java
@@ -1,3 +1,14 @@
+/* SubAddMenuActivity
+ *
+ *  Feb 05, 2018
+ *
+ *  Copyright © 2018 patenio CMPUT301, University of Alberta - All Rights Reserved.
+ *  You may use, distribute or modify this code under terms and conditions of Code
+ *  of Student Behaviour at
+ *  University of Alberta.
+ *  You can find a copy of the license in this project. Otherwise, please contact patenio@ualberta.ca
+ */
+
 package ca.ualberta.cs.patenio_subbook;
 
 import android.content.Context;
